cb8e847184fd53e32c04284945284cd068583da9
yummyweb <antriksh.timepass@gmail.com>
2022-02-09 21:15:39 +0530

    feat: add metis contract which should probably work

--- contracts/MWWRegistarMetis.sol
+++ contracts/MWWRegistarMetis.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4 <0.9.0;
 
-import "witnet-solidity-bridge/contracts/interfaces/IWitnetPriceRouter.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./DIAOracleV2.sol";
 import "./MWWRegistarBase.sol";
 import "hardhat/console.sol";
 
 contract MWWRegistarMetis is MWWRegistarBase {
-	IWitnetPriceRouter public router;
+	DIAOracleV2 public oracle;	
 
-    constructor(address[] memory acceptableTokenAddresses) MWWRegistarBase(acceptableTokenAddresses) {
-		router = IWitnetPriceRouter(0xD39D4d972C7E166856c4eb29E54D3548B4597F53);
+    constructor(address[] memory acceptableTokenAddresses) MWWRegistarBase(acceptableTokenAddresses) {}
+
+	function setPriceFeed(address priceFeed) public onlyOwner {
+		oracle = new DIAOracleV2(priceFeed);
 	}
 
-    function getNativeConvertedValue(int256 usdPrice) public view override returns (int256) {
-    	(_price,,) = router.valueFor(bytes32(0x4ba45817));
+    function getNativeConvertedValue(uint256 usdPrice) public view override returns (uint256) {
+		(value, timestamp) = oracle.getValue("Metis/USD");
+		uint256 absoluteValue = value / 100000000;
+		uint256 perUSD = 1 / absoluteValue;
+		return perUSD * usdPrice;
 	}
-    
 }
